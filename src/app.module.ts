2877f07e244f8f3f485e279806546e5794f3948e
mchiarello <m.chiarello1854@gmail.com>
2025-06-01 19:20:50 +0200

    populated database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { PostsService } from './post.service';
+import { UsersService } from './user.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [CatsModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService, PrismaService, PostsService],
 })
 export class AppModule {}
