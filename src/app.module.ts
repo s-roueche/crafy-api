a9ae39500011842a27ae6f63b7b60bf733f2c388
mchiarello <m.chiarello1854@gmail.com>
2025-06-02 17:02:15 +0200

    refined datatable

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { PostsService } from './post.service';
 import { UsersService } from './user.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [CatsModule, ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, UsersService, PrismaService, PostsService],
 })
