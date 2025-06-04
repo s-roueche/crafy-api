15c1b686e5a1b302e4e780866f8f968af2db03b4
mchiarello <m.chiarello1854@gmail.com>
2025-06-04 11:40:49 +0200

    tests user module

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { Prisma, User } from '@prisma/client';
 
 describe('UserController', () => {
   let controller: UserController;
 
+  const mockUserService = {
+    getUser: jest.fn(),
+    getAllUsers: jest.fn(),
+    createUser: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [{ provide: UserService, useValue: mockUserService }],
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getUserById', () => {
+    it('should return a user if found', async () => {
+      const user: User = { id: '1' };
+      mockUserService.getUser.mockResolvedValue(user);
+
+      const result = await controller.getUserById(user.id);
+      expect(mockUserService.getUser).toHaveBeenCalledWith(user);
+      expect(result).toBe(user);
+    });
+
+    it('should throw an error if user not found', async () => {
+      mockUserService.getUser.mockResolvedValue(null);
+
+      await expect(controller.getUserById('1')).rejects.toThrow(
+        'User not found',
+      );
+    });
+  });
+
+  describe('getAllUsers', () => {
+    it('should return an array of users if there are any', async () => {
+      const users: User[] = [{ id: '1' }, { id: '2' }, { id: '3' }];
+      mockUserService.getAllUsers.mockResolvedValue(users);
+
+      const result = await controller.getAllUsers();
+      expect(mockUserService.getAllUsers).toHaveBeenCalled();
+      expect(result).toBe(users);
+    });
+
+    it('should return an empty array of there are no users', async () => {
+      mockUserService.getAllUsers.mockResolvedValue([]);
+
+      const result = await controller.getAllUsers();
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create and return a new user', async () => {
+      const createdUser: User = { id: '1' };
+      const userInput: Prisma.UserCreateInput = { id: '1' };
+      mockUserService.createUser.mockResolvedValue(createdUser);
+
+      const result = await controller.createUser(userInput);
+      expect(mockUserService.createUser).toHaveBeenCalledWith(userInput);
+      expect(result).toBe(createdUser);
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,91 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '../prisma.service';
+import { Prisma, User } from '@prisma/client';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+
+  const mockPrismaService = {
+    user: {
+      findUnique: jest.fn(),
+      findMany: jest.fn(),
+      create: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return a user if found', async () => {
+      const user: User = { id: '1' };
+      mockPrismaService.user.findUnique.mockResolvedValue(user);
+
+      const result = await userService.getUser({ id: '1' });
+      expect(mockPrismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { id: '1' },
+      });
+      expect(result).toBe(user);
+    });
+
+    it('should return null if user not found', async () => {
+      mockPrismaService.user.findUnique.mockResolvedValue(null);
+
+      const result = await userService.getUser({ id: '2' });
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('getAllUsers', () => {
+    it('should return an array of users', async () => {
+      const users: User[] = [{ id: '1' }, { id: '2' }];
+      mockPrismaService.user.findMany.mockResolvedValue(users);
+
+      const result = await userService.getAllUsers();
+      expect(mockPrismaService.user.findMany).toHaveBeenCalled();
+      expect(result).toBe(users);
+    });
+
+    it('should return an empty array if no users found', async () => {
+      mockPrismaService.user.findMany.mockResolvedValue([]);
+
+      const result = await userService.getAllUsers();
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create and return a new user', async () => {
+      const userInput: Prisma.UserCreateInput = { id: '3' };
+
+      const createdUser: User = {
+        id: '3',
+      };
+
+      mockPrismaService.user.create.mockResolvedValue(createdUser);
+
+      const result = await userService.createUser(userInput);
+      expect(mockPrismaService.user.create).toHaveBeenCalledWith({
+        data: userInput,
+      });
+      expect(result).toBe(createdUser);
+    });
   });
 });
